9c26008c00072c327ae4363365932371bb4cb266
tom jielis van wijgerden <tomjielisvanwijgerden@MacBook-Pro-van-tom.local>
2025-08-21 23:19:15 +0200

    fix

--- config/cors.php
+++ config/cors.php
@@ -1,23 +1,9 @@
 <?php
 
 return [
-
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => [
-        'http://192.168.178.182:3000',
-    ],
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
-    'supports_credentials' => true,
-
+       'paths' => ['api/*', 'sanctum/csrf-cookie'],
+       'allowed_methods' => ['*'],
+       'allowed_origins' => ['http://localhost:3000'],
+       'allowed_headers' => ['*'],
+       'supports_credentials' => true,
 ];
