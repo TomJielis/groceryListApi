7cd675cc80987676a9e2d0df1df4d4a83cac8035
tom jielis van wijgerden <tomjielisvanwijgerden@MacBook-Pro-van-tom.local>
2025-10-12 19:06:21 +0200

    fix

--- release.php
+++ release.php
@@ -2,7 +2,7 @@
 // release.php
 
 $commands = [
-    'composer install',
+    'sudo -u www-data composer install',
     'php artisan migrate',
     'php artisan db:seed',
     'php artisan optimize:clear',
