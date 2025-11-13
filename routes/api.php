c2d7d1b48d4d9ee289e85d3b7a691532031b63b3
Tom Jielis van Wijgerden <tomvanwijgerden@gmail.com>
2025-11-13 21:01:34 +0100

    wip for approving new terms

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,7 @@ Route::post('/reset-password', [AuthController::class, 'resetPassword'])->withou
 Route::post('/set-new-password', [AuthController::class, 'setNewPassword'])->withoutMiddleware('auth:sanctum');
 Route::post('/me', [AuthController::class, 'me']);
 Route::post('/update-language', [AuthController::class, 'updateLanguage']);
+Route::post('/approve-terms', [AuthController::class, 'approveTerms']);
 Route::post('/update', [AuthController::class, 'update']);
 Route::post('/deactivate', [AuthController::class, 'deactivate']);
 Route::get('/verify-email/{hash}', [AuthController::class, 'verifyUser'])->withoutMiddleware('auth:sanctum');
