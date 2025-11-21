d5a2a7be77ba24cdefe4d739ea2f1300b4be8111
Tom Jielis van Wijgerden <tomvanwijgerden@gmail.com>
2025-11-21 18:10:51 +0100

    Revert "wip for the responsive total amount"
    This reverts commit ec6c90bfd88426975e187b06326e42f4f4019e15.

--- app/Http/Controllers/GroceryListItemController.php
+++ app/Http/Controllers/GroceryListItemController.php
@@ -108,19 +108,7 @@ class GroceryListItemController extends Controller
     public function decrease(Request $request, GroceryListItem $listItem): \Illuminate\Http\JsonResponse
     {
         $amount = $request->get('amount', 1);
-        if($amount === 0)
-        {
-            $listItem->quantity = 1;
-            $listItem->checked = true;
-            $listItem->save();
-            return response()->json([
-                'data' => $listItem,
-            ]);
-        }
-
-        $listItem->quantity = $amount;
-        $listItem->save();
-
+        $listItem->decrement('quantity', $amount);
 
         return response()->json([
             'data' => $listItem,
